c362f391eb3079e5b2bd966c1e4252d795b1610c
Rina Inada <indrn@icloud.com>
2023-12-18 12:12:48 -0800

    Add contents

---
+++ src/components/Social.jsx
@@ -0,0 +1,11 @@
+export default function Social() {
+  return (
+    <section className="home__social">
+      <div className="home__footer__center">
+        <button>LinkedIn</button>
+        <button>Github</button>
+        <button>Discord</button>
+      </div>
+    </section>
+  );
+}
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,12 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './styles/Nav.css'
+import './styles/Home.css'
+import './styles/Skills.css'
+import './styles/Contact.css'
+import './styles/Footer.css'
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
