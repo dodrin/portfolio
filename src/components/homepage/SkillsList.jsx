7280f68c99ac7a8772fd5055a2f08c2dc2c0a379
Rina Inada <indrn@icloud.com>
2024-01-18 21:38:01 -0800

    Edit skills

--- src/components/homepage/SkillsList.jsx
+++ src/components/homepage/SkillsList.jsx
@@ -1,17 +1,23 @@
 import { skills } from "../../constants/skillsData";
 
 export default function SkillsList() {
-  
-  const skillsItems = skills.map((skill) => (
-    <li key={skill}>{skill}</li>
-  ));
+  const skillsItems = skills.map((skill) => skill.toLowerCase() + ",");
+  const skillsString = skillsItems.join("");
 
   return (
     <section className="home__skills">
       <h2>Skills</h2>
-      <ul>
-        {skillsItems}
-      </ul>
+      <p>
+        I am always eager to learn more about my current stack and explore new
+        technologies that could expand my skill set, enhance my problem-solving
+        capabilities, and keep me at the forefront of industry trends.
+      </p>
+      <p align="center">
+        <img
+          src={`https://skillicons.dev/icons?i=${skillsString}`}
+          alt="skills icons"
+        />
+      </p>
     </section>
   );
 }
