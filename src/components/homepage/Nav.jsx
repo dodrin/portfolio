c408ff855cdd6ba9c81a863fabdc46d2ac9a9b53
dodrin <indrn@icloud.com>
2024-02-27 18:01:12 -0800

    Add links

--- src/components/homepage/Nav.jsx
+++ src/components/homepage/Nav.jsx
@@ -1,10 +1,12 @@
 import Logo from "../../constants/logo";
 
 export default function Nav() {
-  
   return (
     <nav className="home__nav">
-      <Logo className="nav-logo" color={"#A65958"}/>
+      <Logo className="nav-logo" color={"#A65958"} />
+      <div className="nav-menu">
+        <a href="#contact">Contact</a>
+      </div>
     </nav>
   );
 }
